0f470d7d153f32bbb0960974c1f92b8c3a4df940
Theo De Castro Pinto <tdcpinto@emi.u-bordeaux.fr>
2020-10-08 09:35:41 +0200

    Ajout de la classe Decoder

---
+++ src/ProblemDecoder.py
@@ -0,0 +1,12 @@
+"""
+This module is used to decode the given problems. 
+See exampls here: https://github.com/medrimonia/ssl_defender_viewer
+"""
+
+class Decoder:
+
+    """
+    Decoder is used to decode an input problem and store the important values in dedicated members.
+    """
+    def __init__(self, decoding_function):
+        pass
