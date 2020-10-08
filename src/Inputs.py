d5f85e278410589680c0007df60d457f0d915489
Theo De Castro Pinto <tdcpinto@emi.u-bordeaux.fr>
2020-10-08 09:46:39 +0200

    Ajout de la classe Inputs utilisée pour stocker tous les Inputs du problème de manière standard

---
+++ src/Inputs.py
@@ -0,0 +1,9 @@
+"""
+This module is used to decode the given problems. 
+See exampls here: https://github.com/medrimonia/ssl_defender_viewer
+"""
+
+class Inputs:
+
+    def __init__(self):
+        pass
--- src/ProblemDecoder.py
+++ src/ProblemDecoder.py
@@ -1,3 +1,5 @@
+import Inputs
+
 """
 This module is used to decode the given problems. 
 See exampls here: https://github.com/medrimonia/ssl_defender_viewer
@@ -9,4 +11,26 @@ class Decoder:
     Decoder is used to decode an input problem and store the important values in dedicated members.
     """
     def __init__(self, decoding_function):
-        pass
+        """
+        Construct a new 'Decoder' object.
+
+        :param decoding_function: The function to use to decode the given file (should be different depending on its format).
+        :return: returns nothing
+        """
+
+        # This member is a function, it should take a file as an argument
+        self.decoding_function = decoding_function
+
+        # Used to store the result of the decoding function
+        # The output should be standardized (not thought about yet)
+        self.result = Inputs.Inputs()
+
+    def decode(self, file):
+        """
+        Decodes the given file and stores its result in the corresponding member. 
+        It deletes any previously stored result.
+
+        :param file: The file to decode (currently supported: Nothing)
+        :return: Returns the resulting 'Input' object (or None if something went wrong)
+        """
+        self.result = self.decoding_function(file)
